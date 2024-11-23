e0fc2fe0136c821a029b2db157c2a1000bcd36c6
Artyom.Evsyukov <artyom.evsyukov@gmail.com>
2024-11-23 12:01:44 +0300

    .vscode eslint prettier

--- Purple/11-react-1/.eslintrc.cjs
+++ Purple/11-react-1/.eslintrc.cjs
@@ -1,23 +1,27 @@
 /* eslint-env node */
 
 module.exports = {
-  env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:react/jsx-runtime',
-    'plugin:react-hooks/recommended'
-  ],
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true }
-    ],
-	'semi': ['error', 'always', { 'omitLastInOneLineBlock': false}],
-	'comma-dangle': ['error', 'never'],
-	quotes: ['error', 'single']
-  }
+	env: { browser: true, es2020: true, },
+	extends: [
+		"eslint:recommended",
+		"plugin:react/recommended",
+		"plugin:react/jsx-runtime",
+		"plugin:react-hooks/recommended",
+	],
+	parserOptions: { ecmaVersion: "latest", sourceType: "module", },
+	settings: { react: { version: "18.2", }, },
+	plugins: ["react-refresh",],
+	rules: {
+		"react-refresh/only-export-components": [
+			"warn",
+			{ allowConstantExport: true, },
+		],
+		semi: ["error", "always", { omitLastInOneLineBlock: false, },],
+		// "comma-dangle": ["error", "never",], //висящая запятая
+		"comma-dangle": ["error", "always",], //висящая запятая
+		quotes: ["error", "double",],
+		// quotes: ['error', 'single'],
+		"react/prop-type":[0,],
+		"indent": ["error", "tab",],
+	},
 };
--- Purple/11-react-1/src/App.jsx
+++ Purple/11-react-1/src/App.jsx
@@ -1,10 +1,20 @@
-import './App.css';
+import "./App.css";
+import Button from "./copmponents/Button/button";
 
 function App() {
-
-  return (
-    <div>Проект</div>
-  );
+	return (
+		<>
+			<a
+				href="https://sdfsdf.ru"
+				target="_blank"
+				className="dsfsd" rel="noreferrer" 
+       
+			></a>
+			<h1>Заголовок</h1>
+			<p>Какой-то текст</p>
+			<Button />
+		</>
+	);
 }
 
 export default App;
