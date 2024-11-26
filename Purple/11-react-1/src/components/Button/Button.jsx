f0fa8dc33d292fbd3c8f24934f6840ee6a5cef2f
Artyom.Evsyukov <artyom.evsyukov@gmail.com>
2024-11-26 15:00:05 +0300

    useState

--- Purple/11-react-1/src/components/Button/Button.jsx
+++ Purple/11-react-1/src/components/Button/Button.jsx
@@ -1,12 +1,17 @@
 import './Button.css';
+import { useState } from 'react';
 
 function Button() {
+    //  let text = 'Сохронить';
+    const [text, setText] = useState('Save');
+    console.log('rerender');
 
-	 let text = 'Сохронить';
-     const clicked = () => {
-         console.log('text:', text);
-     };
-	 
+    const clicked = () => {
+        // setText('Close');
+        setText((t) => t + '!');
+        console.log('text:', text);
+    };
+
     return (
         <button onClick={clicked} className="button accent">
             {text}
