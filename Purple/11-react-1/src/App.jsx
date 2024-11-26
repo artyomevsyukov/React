8ee035168fd574d6c97b487d65241223febad3bc
Artyom.Evsyukov <artyom.evsyukov@gmail.com>
2024-11-26 14:22:11 +0300

    Композиция CardButton

--- Purple/11-react-1/src/App.jsx
+++ Purple/11-react-1/src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css';
 import Button from './components/Button/Button';
 import JournalItem from './components/JournalItem/JournalItem';
+import CardButton from './components/CardButton/CardButton';
 
 function App() {
 	const data = [
@@ -31,25 +32,32 @@ function App() {
 			<h1>Заголовок</h1>
 			<p>Какой-то текст</p>
 			<Button />
-			{/* <JournalItem
-				title={data[0].title}
-				text={data[0].text}
-				date={data[0].date}
-			/>
-			<JournalItem
-				title={data[1].title}
-				text={data[1].text}
-				date={data[1].date}
-			/> */}
-			{/* <JournalItem data={data[0]}/>
-			<JournalItem data={data[1]}/>
-			<JournalItem data={data[2]}/> */}
+			<CardButton>
+				{/* <JournalItem
+					title={data[0].title}
+					text={data[0].text}
+					date={data[0].date}
+				/>
+				<JournalItem
+					title={data[1].title}
+					text={data[1].text}
+					date={data[1].date}
+				/> */}
+
+				{data.map((item, index) => (
+					<JournalItem key={index} data={item} />
+				))}
+
+				{/* <JournalItem data={data[0]} />
+				<JournalItem data={data[1]} />
+				<JournalItem data={data[2]} /> */}
+			</CardButton>
 			{/* {data.map((item, index) => {
 				return <JournalItem key={index} data={item} />;
 			})} */}
-			{data.map((item, index) => (
+			{/* {data.map((item, index) => (
 				<JournalItem key={index} data={item} />
-			))}
+			))} */}
 		</>
 	);
 }
