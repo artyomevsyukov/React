2b3796e176e2bfe0f7b603f1c8d99c7830eeff5a
Artyom.Evsyukov <artyom.evsyukov@gmail.com>
2024-11-29 11:45:06 +0300

    Стилизация валидации через style={} в JSX

--- Purple/11-react-1/src/components/JournalForm/JournalForm.jsx
+++ Purple/11-react-1/src/components/JournalForm/JournalForm.jsx
@@ -1,16 +1,9 @@
 import './JournalForm.css';
-// import { useState } from 'react';
 import Button from '../Button/Button';
 import { useState } from 'react';
 
 function JournalForm({ submit }) {
-    // const [inputData, setInputData] = useState('');
-
-    // const inputChange = e => {
-    //     setInputData(e.target.value);
-    //     console.log(inputData);
-    // };
-    const [validateFormState, setValidateFormState] = useState({
+    const [formValidateState, setFormValidateState] = useState({
         title: true,
         text: true,
         date: true,
@@ -23,60 +16,78 @@ function JournalForm({ submit }) {
         console.log(formProps);
 
         let isFormValid = true;
+        console.log('formValidateState.date: ', formValidateState.date);
+        console.log('formProps.date: ', formProps.date);
         if (!formProps.title?.trim().length) {
-            setValidateFormState(state => ({ ...state, title: false }));
+            setFormValidateState(state => ({ ...state, title: false }));
             isFormValid = false;
+        } else {
+            setFormValidateState(state => ({ ...state, title: true }));
         }
         if (!formProps.text?.trim().length) {
-            setValidateFormState(state => ({ ...state, text: false }));
+            setFormValidateState(state => ({ ...state, text: false }));
             isFormValid = false;
+        } else {
+            setFormValidateState(state => ({ ...state, text: true }));
         }
         if (!formProps.date) {
-            setValidateFormState(state => ({ ...state, date: false }));
+            setFormValidateState(state => ({ ...state, date: false }));
             isFormValid = false;
+        } else {
+            setFormValidateState(state => ({ ...state, date: true }));
         }
 
         if (!isFormValid) {
             return;
         }
-
         submit(formProps);
     };
 
     return (
         <>
             <form className="journal-form" onSubmit={addJournalItem}>
-                <input type="text" name="title" />
-                <input type="date" name="date" />
                 <input
                     type="text"
-                    name="tag"
-                    /*   value={inputData}
-                    onChange={inputChange} */
+                    name="title"
+                    style={{
+                        border: formValidateState.title
+                            ? undefined
+                            : '1px solid red',
+                    }}
                 />
-                <textarea name="text" id="" cols="30"></textarea>
-                {/* <Button
-                    type="button"
-                    text={'Сохронить'}
-                    // onClick={() => {
-                    //     console.log('object');
-                    // }}
-                /> */}
-                {/* Кнопка "Отправить форму" */}
-                {/* <Button text="Сохранить" /> */}
+                <input
+                    type="date"
+                    name="date"
+                    style={{
+                        border: formValidateState.date
+                            ? undefined
+                            : '1px solid red',
+                    }}
+                />
+                <input type="text" name="tag" />
+                <textarea
+                    name="text"
+                    id=""
+                    cols="30"
+                    style={{
+                        border: formValidateState.text
+                            ? undefined
+                            : '1px solid red',
+                    }}
+                ></textarea>
+
                 <Button
                     text="Сохранить"
                     type="submit"
                     onClick={() => console.log('На нас нажали')}
                 />
-                {/* Кнопка "Просто действие" */}
+
                 <Button
                     text="Другое действие"
                     onClick={() => console.log('Это не отправка формы')}
                     type="button"
                 />
 
-                {/* Кнопка "Сброс формы" */}
                 <Button text="Сбросить" type="reset" />
             </form>
         </>
