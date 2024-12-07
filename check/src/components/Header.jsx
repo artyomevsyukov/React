7de1014d396dfaa3ef97e2f6dd4fe2a03d9cdb0e
Artyom.Evsyukov <artyom.evsyukov@gmail.com>
2024-12-07 13:06:16 +0300

    test ccs module

--- check/src/components/Header.jsx
+++ check/src/components/Header.jsx
@@ -1,25 +1,24 @@
+import CN from "classnames"
+import "../App.css"
 import styles from "./Header.module.scss"
-import { useEffect, useState } from 'react'
 
+import { useEffect, useState } from "react"
 
 function Header() {
-    
-const [count, setCount] = useState(0);
-const [count2, setCount2] = useState(0);
+  const [count, setCount] = useState(0)
+  const [count2, setCount2] = useState(0)
 
-useEffect(() => {
-  document.title = `Вы нажали ${count}`;
-}, [count]);
+  useEffect(() => {
+    document.title = `Вы нажали ${count}`
+  }, [count])
 
-    return <div className={styles.header}>
-        <h1>Заголовок был нажат {count} раз</h1>
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <button onClick={() => setCount2((count2) => count2 + 1)}>
-          count2 is {count2}
-        </button>
-    </div>;
+  return (
+    <div /* className={`title ${styles.header} `} */>
+      <h1 className={CN("title", styles.header__title, styles.header)}>Заголовок был нажат {count} раз</h1>
+      <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
+      <button onClick={() => setCount2((count2) => count2 + 1)}>count2 is {count2}</button>
+    </div>
+  )
 }
 
-export default Header;
+export default Header
