5dc0dde9bec908150a0648eb8f4302d06c29ed21
Artyom.Evsyukov <artyom.evsyukov@gmail.com>
2024-12-12 11:35:50 +0300

    react-router useNavigate()

--- Bogdan/react-router/src/components/Courses/SingleCouse.jsx
+++ Bogdan/react-router/src/components/Courses/SingleCouse.jsx
@@ -1,21 +1,42 @@
 import CN from "classnames"
-import { Link, useParams } from "react-router-dom"
+import { useEffect } from "react"
+import { Link, useNavigate, useParams } from "react-router-dom"
 import { courses } from "../../data/courses"
 import styles from "./Courses.module.css"
+// import NotFound from "../NotFound"
 
 function SingleCouse() {
   const params = useParams()
-  console.log("params: ", params)
+  // console.log("params: ", params)
+  const navigate = useNavigate()
   const course = courses.find((course) => course.slug === params.courseSlug)
-  console.log("course: ", course)
+  // console.log("course: ", course)
+
+  // Показывает Not Found и ссылку на все курсы
+  // if (!course) {
+  //   return (
+  //     <>
+  //       <NotFound />
+  //       <Link to=".." relative="path">
+  //         All Courses
+  //       </Link>
+  //     </>
+  //   )
+  // }
+
+  useEffect(() => {
+    if (!course) {
+      navigate("..", { relative: "path" })
+    }
+  }, [course, navigate])
+
   return (
     <div
       //   className={`${styles.coursesWrapper} ${styles.coursesItem}`}
-      className={CN(styles.coursesItem, styles.coursesWrapper)}
-      key={course.id}>
-      <h2>{course.title}</h2>
-      <div>{course.slug}</div>
-      <div>{course.id}</div>
+      className={CN(styles.coursesItem, styles.coursesWrapper)}>
+      <h2>{course?.title}</h2>
+      <div>{course?.slug}</div>
+      <div>{course?.id}</div>
       {/* <Link to="/courses">All Courses</Link> */}
       <Link to=".." relative="path">
         All Courses
