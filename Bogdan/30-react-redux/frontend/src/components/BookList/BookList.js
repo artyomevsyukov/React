22ea66ad9cabecdcf53236d31a9291b0fdb76318
Artyom.Evsyukov <artyom.evsyukov@gmail.com>
2024-12-23 14:02:35 +0300

    start BookForm

---
+++ Bogdan/30-react-redux/frontend/src/components/BookList/BookList.js
@@ -0,0 +1,9 @@
+function BookList() {
+  return (
+    <div className="app-block book-form">
+      <h2>Book List</h2>
+    </div>
+  )
+}
+
+export default BookList
---
+++ Bogdan/30-react-redux/frontend/src/components/Filter/Filter.js
@@ -0,0 +1,9 @@
+function Filter() {
+  return (
+    <div className="app-block book-form">
+      <h2>Filters</h2>
+    </div>
+  )
+}
+
+export default Filter
