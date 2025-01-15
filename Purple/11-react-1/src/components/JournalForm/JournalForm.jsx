2a80e369ca8f4411e50f6e7eb86ed2830271b274
Artyom.Evsyukov <artyom.evsyukov@gmail.com>
2025-01-15 11:01:21 +0300

    purple useReducer

--- Purple/11-react-1/src/components/JournalForm/JournalForm.jsx
+++ Purple/11-react-1/src/components/JournalForm/JournalForm.jsx
@@ -1,66 +1,37 @@
 import styles from './JournalForm.module.css'
 import Button from '../Button/Button'
-import { useEffect, useState } from 'react'
+import { useEffect, useReducer } from 'react'
 import CN from 'classnames'
+import { INITIAL_STATE, formReducer } from './JournalFrom.state'
 
 function JournalForm({ submit }) {
-    const INITIAL_STATE = {
-        title: true,
-        post: true,
-        date: true,
-    }
-
-    const [formValidateState, setFormValidateState] = useState(INITIAL_STATE)
+    const [formState, dispatchForm] = useReducer(formReducer, INITIAL_STATE)
+    const { isValid, values, isFormReadyToSubmit } = formState
 
     useEffect(() => {
         let timerID
-        if (
-            !formValidateState.date ||
-            !formValidateState.post ||
-            !formValidateState.title
-        ) {
+        if (!isValid.date || !isValid.post || !isValid.title) {
             timerID = setTimeout(() => {
-                console.log('Очистка состояния')
-                setFormValidateState(INITIAL_STATE)
+                dispatchForm({ type: 'RESET_VALIDITY' })
             }, 2000)
         }
         return () => {
             clearTimeout(timerID)
         }
-    }, [formValidateState])
+    }, [isValid])
+
+    useEffect(() => {
+        if (isFormReadyToSubmit) {
+            submit(values)
+        }
+    }, [isFormReadyToSubmit, values])
 
     const addJournalItem = e => {
         e.preventDefault()
         const formData = new FormData(e.target)
         const formProps = Object.fromEntries(formData)
         // console.log(formProps);
-
-        let isFormValid = true
-        // console.log("formValidateState.date: ", formValidateState.date);
-        // console.log("formProps.date: ", formProps.date);
-        if (!formProps.title?.trim().length) {
-            setFormValidateState(state => ({ ...state, title: false }))
-            isFormValid = false
-        } else {
-            setFormValidateState(state => ({ ...state, title: true }))
-        }
-        if (!formProps.post?.trim().length) {
-            setFormValidateState(state => ({ ...state, post: false }))
-            isFormValid = false
-        } else {
-            setFormValidateState(state => ({ ...state, post: true }))
-        }
-        if (!formProps.date) {
-            setFormValidateState(state => ({ ...state, date: false }))
-            isFormValid = false
-        } else {
-            setFormValidateState(state => ({ ...state, date: true }))
-        }
-
-        if (!isFormValid) {
-            return
-        }
-        submit(formProps)
+        dispatchForm({ type: 'SUBMIT', payload: formProps })
     }
 
     return (
@@ -72,7 +43,7 @@ function JournalForm({ submit }) {
                         name="title"
                         // className={`${styles['input']} ${formValidateState.title ? '' : styles['invalid']}`}
                         className={CN(styles['input-title'], {
-                            [styles['invalid']]: !formValidateState.title,
+                            [styles['invalid']]: !isValid.title,
                         })}
                     />
                 </div>
@@ -87,7 +58,7 @@ function JournalForm({ submit }) {
                         name="date"
                         // className={`${styles['input']} ${formValidateState.date ? '' : styles['invalid']}`}
                         className={CN(styles['input'], {
-                            [styles['invalid']]: !formValidateState.date,
+                            [styles['invalid']]: !isValid.date,
                         })}
                     />
                 </div>
@@ -111,7 +82,7 @@ function JournalForm({ submit }) {
                     spellCheck="true"
                     // className={`${styles['input']} ${formValidateState.text ? '' : styles['invalid']}`}
                     className={CN(styles['input'], {
-                        [styles['invalid']]: !formValidateState.post,
+                        [styles['invalid']]: !isValid.post,
                     })}
                 ></textarea>
 
