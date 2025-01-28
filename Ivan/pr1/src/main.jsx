c7c807cc06553536bddbeba824fbe1596cb56878
Artyom.Evsyukov <artyom.evsyukov@gmail.com>
2025-01-28 14:29:14 +0300

    finish

--- Ivan/pr1/src/main.jsx
+++ Ivan/pr1/src/main.jsx
@@ -1,8 +1,11 @@
 import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap"
+import "@fortawesome/fontawesome-free/css/all.min.css"
 import "./index.css"
-// import App from "./App.jsx"
-import App from "./App2.jsx"
+import App from "./App.jsx"
+// import App from "./App2.jsx"
 // import App3 from "./App3.jsx"
 
 createRoot(document.getElementById("root")).render(
