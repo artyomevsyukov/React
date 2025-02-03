50d7c7e8e3843056ca475d9028f0b3d1fc7680fa
Artyom.Evsyukov <artyom.evsyukov@gmail.com>
2025-02-03 11:34:54 +0300

    Динамическое изменение Children через Props

--- bootstrap-react/src/App.jsx
+++ bootstrap-react/src/App.jsx
@@ -1,64 +1,100 @@
 import "./App.css"
+import React, { Component } from "react"
+import BootstrapTest1 from "./components/Bootstrap-test-1"
+import BootstrapTest2 from "./components/Bootstrap-test-2"
 
-import { Button, Container, Row, Col, Form, Card } from "react-bootstrap"
-import BasicExample from "./components/Dropdown"
-import Slider from "./components/Slider"
+const DynamicGreeting = (props) => {
+  return (
+    <div className={"mb-3 mt-3 p-3 border border-" + props.color}>
+      {React.Children.map(props.children, (child) => {
+        return React.cloneElement(child, {
+          className: "shadow p-3 m-3 border rounded",
+        })
+      })}
+      {/* {props.children} */}
+    </div>
+  )
+}
+
+class WhoAmI extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      years: 30,
+      text: "Click",
+      position: "",
+    }
+  }
+
+  // nextYear() {
+  //   console.log("Click")
+  // }
+  nextYear = () => {
+    this.setState((state) => ({
+      years: state.years + 1,
+    }))
+  }
+
+  commitInputChange = (e) => {
+    // console.log(e.target.value)
+    this.setState({ position: e.target.value })
+  }
+
+  render() {
+    const { name, surname, link } = this.props
+    const { position, years, text } = this.state
+
+    return (
+      <div>
+        <button onClick={this.nextYear}>{text}</button>
+        <h2>
+          My name is {name}, surname - {surname}, age - {years}, position -{" "}
+          {position}
+        </h2>
+        <a href={link}>My profile</a>
+        <form>
+          <span>Введите должность</span>
+          <input type="text" onChange={this.commitInputChange} />
+        </form>
+        <div></div>
+      </div>
+    )
+  }
+}
 
 function App() {
   return (
-    <>
-      <Container className="mt-5 mb-5">
-        <Row>
-          <Col>
-            <Button>Text</Button>
-            <p className="read-the-docs">
-              Click on the Vite and React logos to learn more
-            </p>
-            <BasicExample>Drop</BasicExample>
-          </Col>
-          <Col>
-            <Form>
-              <Form.Group className="mb-3" controlId="formBasicEmail">
-                <Form.Label>Email address</Form.Label>
-                <Form.Control type="email" placeholder="Enter email" />
-                <Form.Text className="text-muted">
-                  We will never share your email with anyone else.
-                </Form.Text>
-              </Form.Group>
+    <div className="container">
+      <BootstrapTest2
+        left={
+          <DynamicGreeting color={"primary"}>
+            <h2>Text-1</h2>
+            <h2>Text-2</h2>
+          </DynamicGreeting>
+        }
+        right={
+          <DynamicGreeting color={"primary"}>
+            <h2>RIGHT</h2>
+            <h2>RIGHT</h2>
+            <h2>RIGHT</h2>
+            <h2>RIGHT</h2>
+          </DynamicGreeting>
+        }
+      />
+      <h2 className="container">=========================================</h2>
+      <BootstrapTest1 />
+      <h2 className="container">=========================================</h2>
 
-              <Form.Group className="mb-3" controlId="formBasicPassword">
-                <Form.Label>Password</Form.Label>
-                <Form.Control type="password" placeholder="Password" />
-              </Form.Group>
-              <Form.Group className="mb-3" controlId="formBasicCheckbox">
-                <Form.Check type="checkbox" label="Check me out" />
-              </Form.Group>
-              <Button variant="primary" type="submit">
-                Submit
-              </Button>
-            </Form>
-          </Col>
-        </Row>
-        <Row className="mt-5">
-          <Col>
-            <Card style={{ width: "18rem" }}>
-              <Card.Img variant="top" src="../public/3.jpg" />
-              <Card.Body>
-                <Card.Title>Card Title</Card.Title>
-                <Card.Text>
-                  Some quick example text to build on the card title and make up
-                  the bulk of the cards content.
-                </Card.Text>
-                <Button variant="primary">Go somewhere</Button>
-              </Card.Body>
-            </Card>
-          </Col>
-          <Col>
-            <Slider />
-          </Col>
-        </Row>
-      </Container>
-    </>
+      <div className="app">
+        <DynamicGreeting color={"primary"}>
+          <h2>Text-1</h2>
+          <h2>Text-2</h2>
+        </DynamicGreeting>
+
+        <WhoAmI name="John" surname="Smith" link="Facebook.com" />
+        <WhoAmI name="Tom" surname="CL" link="Vk.com" />
+      </div>
+    </div>
   )
 }
 
