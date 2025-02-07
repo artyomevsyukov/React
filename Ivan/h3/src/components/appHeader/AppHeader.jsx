8ad8e03ee2a93f55456e46da9ec78e8c6f78c44a
Artyom.Evsyukov <artyom.evsyukov@gmail.com>
2025-02-07 13:01:49 +0300

    Навигация react-router-dom

--- Ivan/h3/src/components/appHeader/AppHeader.jsx
+++ Ivan/h3/src/components/appHeader/AppHeader.jsx
@@ -1,21 +1,28 @@
 import "./appHeader.scss"
+// import { Link } from "react-router-dom/cjs/react-router-dom.min"
+import { Link, NavLink } from "react-router-dom"
 
 const AppHeader = () => {
   return (
     <header className="app__header">
       <h1 className="app__title">
-        <a href="#">
+        <Link to="#">
           <span>Marvel</span> information portal
-        </a>
+        </Link>
       </h1>
       <nav className="app__menu">
         <ul>
           <li>
-            <a href="#">Characters</a>
+            <NavLink exact to="/" activeStyle={{ color: "#9f0013" }}>
+              Characters
+            </NavLink>
           </li>
           /
           <li>
-            <a href="#">Comics</a>
+            {/* <NavLink exact to="/comics" activeStyle={{ color: "blue" }}> */}
+            <NavLink exact to="/comics" activeClassName="app__link">
+              Comics
+            </NavLink>
           </li>
         </ul>
       </nav>
