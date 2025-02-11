628737d7e8a8dbd69a7378a9413cea7dbbba6dbb
Artyom.Evsyukov <artyom.evsyukov@gmail.com>
2025-02-11 22:00:44 +0300

    fix starter 182-183

--- react-transition/src/main.jsx
+++ react-transition/src/main.jsx
@@ -3,8 +3,7 @@ import { StrictMode } from "react"
 
 import "./index.css"
 import App from "./App"
-// import "bootstrap/dist/css/bootstrap.min.css"
-import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap/dist/css/bootstrap.min.css"
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
