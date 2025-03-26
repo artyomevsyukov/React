252d2e65dfe9278c47af9522d68bb21bd868bd0c
Artyom.Evsyukov <artyom.evsyukov@gmail.com>
2025-03-26 14:30:35 +0300

    React Context - 2

--- check/src/data/recipes.js
+++ check/src/data/recipes.js
@@ -1,26 +1,52 @@
+import { v4 as uuidv4 } from "uuid"
+
 export const initialRecipes = [
   {
-    id: "greek-salad",
-    name: "Name Greek Salad",
-    ingredients: ["tomatoes", "cucumber", "onion", "olives", "feta"],
-    desc: "Способ приготовления",
-  },
-  {
-    id: "hawaiian-pizza",
-    name: "Hawaiian Pizza",
+    id: uuidv4(),
+    name: "Greek Salad",
     ingredients: [
-      "pizza crust",
-      "pizza sauce",
-      "mozzarella",
-      "ham",
-      "pineapple",
+      { name: "tomatoes", amount: "3" },
+      { name: "cucumber", amount: "1" },
+      { name: "onion", amount: "1" },
+      { name: "olives", amount: "20" },
+      { name: "feta", amount: "150g" },
     ],
     desc: "Способ приготовления",
   },
   {
-    id: "hummus",
-    name: "Hummus",
-    ingredients: ["chickpeas", "olive oil", "garlic cloves", "lemon", "tahini"],
+    id: uuidv4(),
+    name: "Hawaiian Pizza",
+    ingredients: [
+      { name: "pizza crust", amount: "1" },
+      { name: "pizza sauce", amount: "100ml" },
+      { name: "mozzarella", amount: "200g" },
+      { name: "ham", amount: "150g" },
+      { name: "pineapple", amount: "100g" },
+    ],
     desc: "Способ приготовления",
   },
+  // {
+  //   id: uuidv4(),
+  //   name: "Vegetable Stir Fry",
+  //   ingredients: [
+  //     { name: "carrot", amount: "2" },
+  //     { name: "bell pepper", amount: "1" },
+  //     { name: "broccoli", amount: "1" },
+  //     { name: "soy sauce", amount: "30ml" },
+  //     { name: "garlic", amount: "2 cloves" },
+  //   ],
+  //   desc: "Способ приготовления",
+  // },
+  // {
+  //   id: uuidv4(),
+  //   name: "Chocolate Cake",
+  //   ingredients: [
+  //     { name: "flour", amount: "200g" },
+  //     { name: "sugar", amount: "150g" },
+  //     { name: "cocoa powder", amount: "50g" },
+  //     { name: "eggs", amount: "3" },
+  //     { name: "butter", amount: "100g" },
+  //   ],
+  //   desc: "Способ приготовления",
+  // },
 ]
