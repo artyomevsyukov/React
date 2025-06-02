f1f654298818a2833af0b0502caa3ac15750f8d8
Artyom.Evsyukov <artyom.evsyukov@gmail.com>
2025-06-02 14:10:47 +0300

    hero_admin_panel start

---
+++ Ivan/redux/hero_admin_panel/src/hooks/http.hook.js
@@ -0,0 +1,39 @@
+import { useCallback } from "react"
+
+export const useHttp = () => {
+  // const [process, setProcess] = useState('waiting');
+
+  const request = useCallback(
+    async (
+      url,
+      method = "GET",
+      body = null,
+      headers = { "Content-Type": "application/json" }
+    ) => {
+      // setProcess('loading');
+
+      const response = await fetch(url, { method, body, headers })
+
+      if (!response.ok) {
+        throw new Error(`Could not fetch ${url}, status: ${response.status}`)
+      }
+
+      const data = await response.json()
+      console.log("data: ", data)
+
+      return data
+    },
+    []
+  )
+
+  // const clearError = useCallback(() => {
+  // setProcess('loading');
+  // }, []);
+
+  return {
+    request,
+    // clearError,
+    // process,
+    // setProcess
+  }
+}
